c707f4757b772298fb492bb9ed404c10671d2d39
KrishothKumar <Krishothkumar@hotmail.com>
2019-06-23 14:06:42 +0530

    Implementing Flask using user_name

---
+++ hello.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "<h1>Hello</h1>"
+
+@app.route('/<user>',methods=['POST'])
+def hello_name(user):
+    return render_template('hello.html', name = user)
+
+# @app.route("/")
+# def hello():
+#     return render_template("hello.html")
+
+# @app.route('/hello/<user>')
+# def hello_name(user):
+#    return render_template('hello.html', name = user)
+
+    # render_template('hello.html')
+# @app.route("/<name>")
+# def hello_name(name):
+#     return "hello {}".format(name)
+
+if __name__== "__main__":
+    app.run()
